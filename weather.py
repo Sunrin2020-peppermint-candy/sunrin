d3deb055b79718b919c42e8d4722634771f2307c
jwp0116 <cong0116@naver.com>
2020-11-07 10:35:16 +0900

    응답 코드 해석 & 출력 / 현재 공개되어 있는 가장 최근 시간 코드 알아내기

--- weather.py
+++ weather.py
@@ -10,6 +10,27 @@ BASE_URL = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst'
 SERVICE_KEY = os.getenv('WEATHER_SERVICE_KEY')
 DATA_TYPE='JSON'
 
+NULL_DATA = {'value': 'NULL'}
+
+# 맑음(1), 구름많음(3), 흐림(4)
+SKY_CODE = {
+  '1': '맑음',
+  '3': '구름많음',
+  '4': '흐림',
+}
+
+# 없음(0), 비(1), 비/눈(2), 눈(3), 소나기(4), 빗방울(5), 빗방울/눈날림(6), 눈날림(7)
+PTY_CODE = {
+  '0': '없음',
+  '1': '비',
+  '2': '비/눈',
+  '3': '눈',
+  '4': '소나기',
+  '5': '빗방울',
+  '6': '빗방울/눈날림',
+  '7': '눈날림',
+}
+
 
 def getVilageFcst(date, time, nx, ny):
   requestUrl = '{}?serviceKey={}&dataType={}&base_date={}&base_time={}&nx={}&ny={}'.format(BASE_URL, SERVICE_KEY, DATA_TYPE, date, time, nx, ny)
@@ -24,14 +45,55 @@ def getVilageFcst(date, time, nx, ny):
     print('ERROR! CODE: {}, MESSAGE: {}'.format(result['header']['resultCode'], result['header']['resultMsg']))
     return
 
-  result = dict(map(lambda item: (item['category'], item['fcstValue']), result['body']['items']['item']))
+  result = dict(map(lambda item: (item['category'], {'value': item['fcstValue'], 'date': item['fcstDate'], 'time': item['fcstTime']}), result['body']['items']['item']))
   print(result)
 
+  print()
+
+  print('강수 확률:', result['POP']['value'])
+  print('강수 형태:', PTY_CODE[result['PTY']['value']])
+
+  print('하늘 상태:', SKY_CODE[result['SKY']['value']])
+
+  print('풍속:', result.get('VVV', NULL_DATA).get('value'), result.get('UUU', NULL_DATA).get('value'))
+  
+  
 
   return result
 
 def getTodayDate():
   return datetime.date.today().strftime('%Y%m%d')
 
+def getNearTime():
+  now = datetime.datetime.now()
+
+  hour = now.hour
+  minute = now.minute
+  time = hour * 100 + minute
+  
+  # - Base_time : 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회)
+  # - API 제공 시간(~이후) : 02:10, 05:10, 08:10, 11:10, 14:10, 17:10, 20:10, 23:10
+  if time >= 2310:
+    return '2300'
+  elif time >= 2010:
+    return '2000'
+  elif time >= 1710:
+    return '1700'
+  elif time >= 1410:
+    return '1400'
+  elif time >= 1110:
+    return '1100'
+  elif time >= 810:
+    return '0800'
+  elif time >= 510:
+    return '0500'
+  elif time >= 210:
+    return '0200'
+  else:
+    # TOOD: ERROR
+    return '0000'
+
 if __name__ == "__main__":
-  getVilageFcst(getTodayDate(), '0500', 60, 126)
+  getVilageFcst(getTodayDate(), getNearTime(), 61, 129)
+
+  print('현재 공개된 가장 최근의 시간코드:', getNearTime())
